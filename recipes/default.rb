89abdd0201af0c1ac3d39aab1f027c5c270a96b7
Glenn Davy <glenn@davy.net.au>
2013-02-19 18:28:41 +1100

    initial commit

---
+++ recipes/default.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook Name:: postgres-db-master
+# Recipe:: default
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+require_recipe "postgresql::package"
+#require_recipe "postgresql::user"
+
