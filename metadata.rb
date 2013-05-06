be83a2087b3cf7efa8a7242f1c42f4ffa40b9829
Glenn Davy <glenn@davy.net.au>
2013-05-06 10:47:44 +1000

    version bump

--- metadata.rb
+++ metadata.rb
@@ -3,5 +3,5 @@ maintainer_email "glenn@reinteractive.net"
 license          "All rights reserved"
 description      "Installs/Configures postgres-db-master"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
+version          "0.0.2"
 depends          "postgresql"
